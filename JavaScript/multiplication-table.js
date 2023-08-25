a1d0364a1aaa8589e63026466acb2e08359a545d
Mukund Sutar <mukundsutar1512@gmail.com>
2023-08-25 22:02:13 +0530

    Added js assignment, and formaated navbar files

---
+++ JavaScript/multiplication-table.js
@@ -0,0 +1,5 @@
+var number = 11;
+console.log("Table of is " + number + ":");
+for (var i = 1; i <= number; i++) {
+    console.log(number + " x " + i + " = " + number * i);
+}
---
+++ JavaScript/pass-fail.js
@@ -0,0 +1,11 @@
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+} else {
+    console.log(num3);
+}
